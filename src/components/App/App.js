8e30297fcd0b85b3d452f08a3b3235cb136c1273
Dustin-Har <harbaughdustin@yahoo.com>
2021-05-10 17:56:10 -0400

    Initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,16 +7,17 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      staff: people.staff
+      staff: people.staff,
+      students: people.students,
     }
   }
 
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-      <h1>Turing Yearbook</h1>
-      </header>
+        <header className="App-header">
+          <h1>Turing Yearbook</h1>
+        </header>
         YOUR CODE GOES HERE
       </div>
     );
